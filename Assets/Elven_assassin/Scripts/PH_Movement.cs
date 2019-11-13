7038e6a65fa624254a5afbe295f178d70dddcff1
CrimsonVixen <JennJP65@yahoo.com>
2019-11-13 15:50:30 -0500

    Setting up Player Model
    Uploading Player model, just need to animate.

---
+++ Assets/Elven_assassin/Scripts/PH_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PH_Movement : MonoBehaviour
+{
+    private Vector3 playerPosition;
+
+    void Start()
+    {
+        playerPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerPosition.z -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerPosition.z += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerPosition.x -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerPosition.x += 0.1f;
+        }
+
+        this.transform.position = playerPosition;
+    }
+}
