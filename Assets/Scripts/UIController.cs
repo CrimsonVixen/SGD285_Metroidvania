4f80a1dce4ad1f859c8d389c774f021d1135fac3
andrewiscool144 <55508319+andrewiscool144@users.noreply.github.com>
2019-11-20 16:41:40 -0500

    UI 2.0
    Redid UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,20 +8,61 @@ public class UIController : MonoBehaviour
     public GameObject startPanel;
     public GameObject creditsPanel;
     public GameObject healthPanel;
+    public GameObject mainWeapon1Panel, isUsingMain1Panel;
+    public GameObject mainWeapon2Panel, isUsingMain2Panel;
+    public GameObject rangedWeapon1Panel, isUsingRanged1panel;
+    public GameObject rangedWeapon2Panel, isUsingRanged2panel;
     public Text hpText;
     public int health = 250;
     public float startHP = 154.15f;
+    public int mainWeaponUsing = 1;
+    public int rangedWeaponUsing = 1;
 
     void Start()
     {
         startPanel.SetActive(true);
         creditsPanel.SetActive(false);
+
+        // healthPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(health, healthPanel.GetComponent<RectTransform>().sizeDelta.y);
+        // healthPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(startHP, healthPanel.GetComponent<RectTransform>().anchoredPosition.y);
+
+        //mainWeapon1Panel.GetComponent<Image>().color = Color.blue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mainWeaponUsing *= -1;
+            if (mainWeaponUsing == 1)
+            {
+                isUsingMain1Panel.SetActive(true);
+                isUsingMain2Panel.SetActive(false);
+            }
+            //else if (mainWeaponUsing )
+            if (mainWeaponUsing == -1)
+            {
+                isUsingMain2Panel.SetActive(true);
+                isUsingMain1Panel.SetActive(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rangedWeaponUsing *= -1;
+            if (rangedWeaponUsing == 1)
+            {
+                isUsingRanged1panel.SetActive(true);
+                isUsingRanged2panel.SetActive(false);
+            }
+            //else if (mainWeaponUsing )
+            if (rangedWeaponUsing == -1)
+            {
+                isUsingRanged2panel.SetActive(true);
+                isUsingRanged1panel.SetActive(false);
+            }
+        }
     }
 
     public void OnPlayButtonClick()
